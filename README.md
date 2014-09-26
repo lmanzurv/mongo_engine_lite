9bd5364de1dbb6118ce16639473d0ae19c34436f
lmazurv <lmanzur@gmail.com>
2014-09-26 13:53:41 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 mongo_engine_lite
 =================
+MongoEngine Lite helps create a lite version of a Mongo database, similar to SQLite to SQL databases. MongoEngine Lite is a wrapper over MongoEngine ORM and persists the information to a JSON file in the project's root.
+
+To connect to the database, use
+
+    from mongoenginelite import connect
+
+    connect('dbname')
+
+Documents and EmbeddedDocuments can be used in the same way that MongoEngine does. Nothing else is necessary.
---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup, find_packages
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='mongoenginelite',
+    version='0.1',
+    packages=find_packages(),
+
+    # Dependencies
+    install_requires = ['mongoengine>=0.8.7'],
+
+    # Metadata for PyPI
+    author='Laura Manzur',
+    author_email='lc.manzur@novcat.com.co',
+    maintainer='Laura Manzur',
+    maintainer_email='lc.manzur@novcat.com.co',
+    description='This is a lite version of MongoEngine ORM that persists the information in a JSON file, similar to SQLite to SQL databases',
+    long_description=README,
+    license='Apache License',
+    url='https://github.com/lmanzurv/mongo_engine_lite',
+    keywords='mongo mongoengine lite',
+    download_url='https://github.com/lmanzurv/mongo_engine_lite',
+    bugtrack_url='https://github.com/lmanzurv/mongo_engine_lite/issues',
+    classifiers=[
+        'License :: OSI Approved :: Academic Free License (AFL)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7'
+    ]
+)
